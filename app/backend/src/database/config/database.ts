2188bd0b00084166eea7d1589ab9dcdd025270bc
Livia Bastos <livialbastos@gmail.com>
2023-09-29 16:39:08 -0300

    req 2 testes de integração

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,7 +2,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '987695441',
+  password: process.env.DB_PASS || '123456',
   database: 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -2,6 +2,7 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import SequelizeTeam from '../database/models/TeamModel'
 
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
@@ -38,8 +39,30 @@ describe('Seu teste', () => {
 
   //   expect(...)
   // });
+  
+const team = { id: 1, teamName: 'Galo', };
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+const teams = [team];
+
+  it('Retorna todos os times', async function() {
+    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(teams);
   });
+
+  it('Retorna um time pelo id', async function() {
+    sinon.stub(SequelizeTeam, 'findByPk').resolves(team as any);
+
+    const { status, body } = await chai.request(app).get('/team/1');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(team);
+  });
+
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
 });
