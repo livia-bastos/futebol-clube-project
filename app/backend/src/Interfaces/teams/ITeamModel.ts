a3187f4d6eb180ca22b31404843cdde0342ad959
Livia Bastos <livialbastos@gmail.com>
2023-09-29 14:40:55 -0300

    requisito 5

--- app/backend/src/Interfaces/teams/ITeamModel.ts
+++ app/backend/src/Interfaces/teams/ITeamModel.ts
@@ -1,5 +1,7 @@
 import { ITeam } from './ITeams';
 
-export interface IteamModel {
+export interface ITeamModel {
   create(data: Partial<ITeam>): Promise<ITeam>,
+  findAll(): Promise<ITeam[]>,
+  findById(id: ITeam['id']): Promise<ITeam | null>
 }
--- app/backend/src/models/TeamModel.ts
+++ app/backend/src/models/TeamModel.ts
@@ -1,15 +1,30 @@
 import SequelizeTeam from '../database/models/TeamModel';
 import { ITeam } from '../Interfaces/teams/ITeams';
-import { IteamModel } from '../Interfaces/teams/ITeamModel';
+import { ITeamModel } from '../Interfaces/teams/ITeamModel';
 import { NewEntity } from '../Interfaces';
 
-export default class TeamModel implements IteamModel {
+export default class TeamModel implements ITeamModel {
   private model = SequelizeTeam;
 
   async create(data: NewEntity<ITeam>): Promise<ITeam> {
     const dbData = await this.model.create(data);
 
     const { id, teamName }: ITeam = dbData;
+    return { id, teamName };
+  }
+
+  async findAll(): Promise<ITeam[]> {
+    const dbData = await this.model.findAll();
+    return dbData.map(({ id, teamName }) => (
+      { id, teamName }
+    ));
+  }
+
+  async findById(id: ITeam['id']): Promise<ITeam | null> {
+    const dbData = await this.model.findByPk(id);
+    if (dbData == null) return null;
+
+    const { teamName }: ITeam = dbData;
     return { id, teamName };
   }
 }
